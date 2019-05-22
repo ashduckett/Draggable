7648d3c42909314b0da7076cd74ae2ce0f37a7a1
Ash Duckett <ash.duckett@outlook.com>
2019-05-22 10:16:03 +0100

    Add ability to enable dragging only for specific areas on the element

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
+class Rect {
+    constructor(top, right, bottom, left) {
+        this.top = top;
+        this.right = right;
+        this.bottom = bottom;
+        this.left = left;
+    }
+}
+
 class Draggable {
 
     getElement() {
         return this.element;
     }
 
-    constructor(height, width, x, y) {
+    constructor(height, width, x, y, options) {
         this.element = document.createElement('div');
         this.element.style.position = 'absolute';
         this.element.style.width = width + 'px';
@@ -13,6 +22,15 @@ class Draggable {
         this.element.style.top = y + 'px';
         this.element.style.background = 'red';
         this.isDragging = false;
+
+        // Options stuff
+        if (options.useConstraints == true) {
+            this.useConstraints = true;
+            this.constraintRect = options.constraintRect;
+        } else if (options.useConstraints == false) {
+            this.useConstraints = false;
+        }
+
         this.setupEventHandlers();
     }
 
@@ -23,10 +41,16 @@ class Draggable {
             const rect = e.target.getBoundingClientRect()
                 
             // Get local coordinates for draggable element. This allows for the relative positioning.
-            // Ya bastard.
-            this.localXDown = e.clientX  - rect.left;
-            this.localYDown = e.clientY  - rect.top;
-            this.isDragging = true
+            const x = this.localXDown = e.clientX  - rect.left;
+            const y = this.localYDown = e.clientY  - rect.top;
+
+            if (this.useConstraints && !(x >= this.constraintRect.left && x <= this.constraintRect.right && y <= this.constraintRect.bottom && y >= this.constraintRect.top)) {
+                this.isDragging = true;
+            } else if (this.useConstraints == false) {
+                this.isDragging = true;
+            }
+         
+
         });
 
         document.addEventListener('mousemove', (e) => {
@@ -43,7 +67,9 @@ class Draggable {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    const d = new Draggable(100, 100, 100,  100);
+    const d = new Draggable(100, 100, 100,  100, {
+        useConstraints: true,
+        constraintRect: new Rect(50, 100, 100, 0)
+    });
     document.body.appendChild(d.getElement());
 });
-
